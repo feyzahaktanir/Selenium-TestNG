82e1b54508e83e37f97ffe3572ddf134210e97f7
feyzahaktanir <feyza.haktanir@hotmail.com>
2022-11-22 12:49:38 +0300

    Selenium Ders 12 - Mouse Actions

---
+++ src/test/java/tests/day12/C02_MouseActions.java
@@ -0,0 +1,35 @@
+package tests.day12;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+import javax.swing.*;
+
+public class C02_MouseActions extends TestBase {
+
+    @Test
+    public void amazonList() throws InterruptedException {
+        // amazon anasayfaya gidin
+        driver.get("https://www.amazon.com");
+
+        Actions action = new Actions(driver);
+
+        // sağ üstte hello, signin elementinin üzerinde mouse'u bekletin
+        WebElement textHello = driver.findElement(By.id("nav-link-accountList-nav-line-1"));
+        action.moveToElement(textHello).perform();
+
+        Thread.sleep(3000);
+
+        // açılan menüde create a list linkine tıklayın
+        WebElement createList = driver.findElement(By.xpath("//span[text()='Create a List']"));
+        action.click(createList).perform();
+
+        // ve new list sayfasının açıldığını test edin
+        Assert.assertTrue(driver.findElement(By.className("al-intro-banner-header")).isDisplayed());
+    }
+
+}
